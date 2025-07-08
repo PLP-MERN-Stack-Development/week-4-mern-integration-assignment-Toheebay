70943ccbc0b274e1d40e0a2837c9421c867b7414
Toheebay <successphu@gmail.com>
2025-07-08 06:41:01 -0700

    Update API baseURL to production Render URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://week-4-mern-integration-assignment-g8yo.onrender.com/api',
 });
 
 // Posts
